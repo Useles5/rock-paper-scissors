9b07e0f312100605c19b2986af4d401fbb27300a
sharathp27 <sharathpoojary270604@gmail.com>
2024-08-27 15:14:40 +0530

    Implement function to get human choice

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // console.log("Rock, Paper, Scissors, Shoot!");
 
-let choice = ["rock", "paper", "scissors"];
+let choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){
-    return choice[randomChoice()];
+    return choices[randomChoice()];
 }
 
 function randomChoice() {
@@ -11,3 +11,27 @@ function randomChoice() {
     return Math.floor(Math.random() * max);
 }
 
+function getHumanChoice() {
+    let humanChoice;
+
+    do {
+        humanChoice = prompt(
+            "Enter your Choice: Rock, Paper, or Scissors?\n" + 
+            "Rock : 0\n" + 
+            "Paper : 1\n" + 
+            "Scissors : 2\n",
+            "0"
+        );
+        humanChoice = Number(humanChoice);
+
+    } while (
+        isNaN(humanChoice) ||
+        !Number.isInteger(humanChoice) ||
+        humanChoice < 0 || 
+        humanChoice > 2
+    );
+
+    return alert(`Your Choice: ${choices[humanChoice]} | Computer Choice: ${getComputerChoice()}`); 
+}
+
+getHumanChoice();
