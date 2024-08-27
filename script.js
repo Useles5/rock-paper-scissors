3cd5f71defdc39868411ac2e3e3413792ab30eca
sharathp27 <sharathpoojary270604@gmail.com>
2024-08-27 12:34:09 +0530

    Implement function for random number from 0 to 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // console.log("Rock, Paper, Scissors, Shoot!");
+
+// function getComputerChoice(){
+    
+// }
+
+function randomChoice() {
+    let max = 3;
+    return Math.floor(Math.random() * max);
+}
+
+console.log(randomChoice());
