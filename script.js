d7f99f7df636b54a68794987ab5441d3fb3395b2
sharathp27 <sharathpoojary270604@gmail.com>
2024-08-27 12:40:38 +0530

    Implement function to get computer choice

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // console.log("Rock, Paper, Scissors, Shoot!");
 
-// function getComputerChoice(){
-    
-// }
+let choice = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    return choice[randomChoice()];
+}
 
 function randomChoice() {
     let max = 3;
     return Math.floor(Math.random() * max);
 }
 
-console.log(randomChoice());
